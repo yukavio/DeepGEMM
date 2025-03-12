6e53c6613d35cc770bb1d04ddebe3b17d2698be2
kavioyu <kaivoyu@gmail.com>
2025-03-12 13:48:02 +0000

    tested

--- deep_gemm/__init__.py
+++ deep_gemm/__init__.py
@@ -3,6 +3,7 @@ import torch
 from . import jit
 from .jit_kernels import (
     gemm_fp8_fp8_bf16_nt,
+    gemm_fp8_fp8_bf16_bw_nt,
     m_grouped_gemm_fp8_fp8_bf16_nt_contiguous,
     m_grouped_gemm_fp8_fp8_bf16_nt_masked,
     ceil_div,
--- deep_gemm/jit_kernels/__init__.py
+++ deep_gemm/jit_kernels/__init__.py
@@ -1,4 +1,5 @@
 from .gemm import gemm_fp8_fp8_bf16_nt
+from .gemm_bw import gemm_fp8_fp8_bf16_bw_nt
 from .m_grouped_gemm import (
     m_grouped_gemm_fp8_fp8_bf16_nt_contiguous,
     m_grouped_gemm_fp8_fp8_bf16_nt_masked
